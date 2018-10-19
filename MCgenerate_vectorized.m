4288aade3002ea9b699d2bea792d989b214429fd
Maximilian Vogler <mvogler@princeton.edu>
2018-10-19 16:35:14 -0400

    Previous to elimination of MCgenerate_vectorized.m which will not be needed.

--- MCgenerate_vectorized.m
+++ MCgenerate_vectorized.m
@@ -1,8 +1,10 @@
-function [ALPHA,RT,ZHS,ZFS,ZHL,ZFL] = MCgenerate_vectorized(S_multiple,MH_small,MF_small,MH_large,MF_large,theta,muT,sigmaT)
-%function [ALPHA] = MCgenerate_vectorized(S_multiple)
+function [ALPHA,RTS,RTL,ZHS,ZFS,ZHL,ZFL] = MCgenerate_vectorized(S_multiple,split_param,outer,MH_small,MF_small,MH_large,MF_large,theta,muT,sigmaT)
+%function [ALPHA,RT] = MCgenerate_vectorized(S_multiple)
 % This code generates productivity parameters as in step 1 and 2 of the
 % algorithm.
 
+if outer == 1                                           % This will be called outside the estimation loop
+    
 % Compute number of sectors 
 cdshares_init = csvread('cdshares_v3.csv');             % Cobb-Douglas shares from external data source.
 
@@ -10,25 +12,31 @@ S_init = length(cdshares_init);                         % Number of sectors in d
 S = S_init*S_multiple;                                  % Number of sectors used (see footnote 56)
 
 % Assign CD-shares across sectors
-cdshares = cdshares_init;      
+ALPHA = cdshares_init;      
 
 for iloop = 1:S_multiple-1;
-    cdshares = [cdshares;cdshares_init(randperm(S_init))];
+    ALPHA = [ALPHA;cdshares_init(randperm(S_init))];
 end
-cdshares = cdshares/S_multiple;
-ALPHA = cdshares;                                       % Rename CD-shares
+ALPHA = ALPHA/S_multiple;
+
+% Split sectors into large and small based on number of CD-share.
+small=(ALPHA<split_param/S);                            
+Nsmall=sum(small);                           
+Nlarge=S-Nsmall;
+
+end
 
 
-% RT=exp(muT+sigmaT*randn(1,S));                 % Make a draw for the relative fundamental comparative advantages from a lognormal distribution for each sector.
-% 
-% split_param=1.25;                              % This determines the split between small and large sectors.
-% ALPHA=cdshares;                                % Rename CD shares in a vector
-% small=(ALPHA<split_param/S);                   % Find indicator for small sectors (why do we have this exact cutoff rule?)
-% Nsmall=sum(small);                             % Number of small sectors
-% Nlarge=S-Nsmall;                               % Number of large sectors
-% 
-% RTS=RT(small);                                 % Relative productivity vector for small sectors
-% RTL=RT(~small);                                % Relative productivity vector for large sectors
+    
+if outer == 0                                           % This will be called inside the estimation loop
+    
+% Given mu_T and sigma_T draw sectoral productivity T_z for each sector z
+% (step 1 of estimation procedure)
+RT = exp(muT+sigmaT*randn(1,S));  
+RTS = RT(small);
+RTL = RT(~small);
+
+
 % 
 % UH0S=exprnd(1,MH_small,Nsmall);                % Draw U of most productive small home shadow firm and spacings in each sector from exponential with mean 1
 % UF0S=exprnd(1,MF_small,Nsmall);                % Draw U of most productive small foreign shadow firm and spacings in each sector from exponential with mean 1
--- Trial.m
+++ Trial.m
@@ -2,6 +2,8 @@ clear all;
 close all;
 clc;
 
+rng(1);
+
 tic
 ALPHA = MCgenerate_vectorized(4);
 toc
