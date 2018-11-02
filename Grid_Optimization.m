93f285dad3cb39143254e196c5bef6c45bb57cf0
Maximilian Vogler <mvogler@princeton.edu>
2018-11-02 13:22:43 -0400

    Grid_Optimization finds 20 best points --> verified to work.
    Grid_Optimization computes the loss function for every parameter combination that is inputted.
    It then finds the best 20 points and reports them.
    Also introduced Verification_Results, which contains all results that have been verified to work exactly as in the oridinal code.

---
+++ Grid_Optimization.m
@@ -0,0 +1,56 @@
+%% This finds the 20 best points from the inputted data file.
+
+%% Housekeeping
+clear all;
+close all;
+clc;
+
+addpath('Data');
+
+%% Load moments from both data and estimation
+
+datamoments = csvread('Data_Moments.csv');
+
+load('estimation_seed1_grid5');
+
+%% Set up weighting matrix W
+
+ref_weight = datamoments;
+ref_weight(14:15) = datamoments(12:13);
+std_indices = [1,3,5,7,9];
+ref_weight(std_indices) = datamoments(std_indices)/3;
+W = diag((1./(ref_weight.^2)));
+
+%% Compute loss function for each parameter combination
+
+NumGrid = size(Momarray,1);
+Loss = zeros(NumGrid,1);
+
+for i = 1:NumGrid
+    Loss(i) = (Momarray(i,:)'-datamoments)'*W*(Momarray(i,:)'-datamoments);
+end
+
+%% Find 20 best points (i.e. with lowest loss)
+
+cutoff = 0.001;
+indicesP = find(Loss<quantile(Loss,cutoff));
+
+num = size(indicesP,1);
+
+if num ~= 20
+    error('We are not looking at the 20 best points')
+end
+
+paramsP = zeros(num,6);
+
+for i = 1:num
+    paramsP(i,1:5) = Paramarray(indicesP(i),1:5);
+    paramsP(i,6) = Loss(indicesP(i));
+end
+
+disp(paramsP)
+
+save('GridOptimization_seed1_grid5','paramsP')
+
+
+
